a009e1229a0d66c6dc3cea5721023ef9ec32d49b
TheRealWeeLad <mateoferntys@gmail.com>
2021-01-05 20:35:53 -0600

    early stages

--- bot.py
+++ bot.py
@@ -15,11 +15,11 @@ def get_prefix(client, message):
 	try:
 		return prefixes[str(message.guild.id)]
 	except KeyError:
-		return ';'
+		return '.'
 
 bot = commands.Bot(command_prefix=get_prefix)
 
-@bot.command(name = 'prefix', help = 'Change the bot\'s prefix, must have a role named  "prefix" to use')
+@bot.command(name = 'prefix', help = 'Change the bot\'s prefix, must have a role named "prefix" to use')
 @commands.has_role('prefix')
 async def change_prefix(ctx, new_prefix):
 	# Check validity
@@ -38,14 +38,38 @@ async def change_prefix(ctx, new_prefix):
 	else:
 		await ctx.send('Prefix too long')
 
+@bot.command(name='balance', help='Check the amount of milk that you have acquired.')
+async def check_balance(ctx, *account):
+	account_to_check = ''
+	try:
+		account_to_check = account[0]
+	except IndexError:
+		account_to_check = ctx.author.mention
+	
+	with open('milk.json', 'r') as f:
+		balances = json.load(f)
+	
+	try:
+		await ctx.send(balances[account_to_check])
+	except KeyError:
+		balances[account_to_check] = 0
+
+		with open('milk.json', 'w') as f:
+			json.dump(balances, f, indent=4)
+		
+		await ctx.send(0)
+
 
 @bot.event
-async def on_message(messsage):
-	await bot.process_commands(message)
+async def on_message(ctx):
+	if ctx.author.id == bot.user.id:
+		return
+
+	await bot.process_commands(ctx)
 
 @bot.event
 async def on_ready():
-	await bot.change_presence(activity=discord.Game(name=';help'))
+	await bot.change_presence(activity=discord.Game(name='.help'))
 	print(f'{bot.user.name} is active')
 
 @bot.event
