4191dfb16ad626993cfe8e7d81a79ce425ed999e
TheRealWeeLad <mateoferntys@gmail.com>
2021-01-11 17:30:02 -0600

    actually finished blackjack this time

--- bot.py
+++ bot.py
@@ -269,7 +269,7 @@ class Gambling(commands.Cog):
 			return
 
 		suits = [':clubs:', ':spades:', ':hearts:', ':diamonds:']
-		numbers = ['A', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+		numbers = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
 		conversion = {2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 'J': 10, 'Q': 10, 'K': 10}
 
 		player_nums = [random.choice(numbers), random.choice(numbers)]
